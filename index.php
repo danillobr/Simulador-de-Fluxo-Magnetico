54205ad59452675729b6d62e69fe6c97b512451c
danillobr <danillorodriguesabreu@gmail.com>
2022-08-11 16:29:53 -0300

    Renaming Files

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <html lang="pt-br">
-   	
-
-   	<head>
-   		<meta charset="UTF-8">
-  		<link rel="stylesheet" type="text/css" href="css/style.css">
-  		<title></title>
-   	</head>
-   	
-
-   	<body>
-   		<?php include("head.php"); ?>
-   		<?php include("footer.php"); ?>
 
 
- 	</body>
- </html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title></title>
+</head>
+
+
+<body>
+	<?php include("header.html"); ?>
+	<?php include("footer.html"); ?>
+</body>
+
+</html>
